e0aaff52f94bce36be4bec96a4d8f22f23b575f0
Angelo Semedo <vamp9278493cv@gmail.com>
2024-12-05 00:39:44 -0100

    inicio

---
+++ Models/DatabaseInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudApp.Models
+{
+	public static class DatabaseInitializer
+	{
+		public static void InitializeDatabase(this IApplicationBuilder app)
+		{
+			using (var serviceScope = app.ApplicationServices.CreateScope())
+			{
+				var context = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
+				context.Database.Migrate(); // Aplica as migrações e cria o banco se não existir
+			}
+		}
+	}
+
+}
